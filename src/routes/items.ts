9dcce8b2bc0dfacd2e910a33e81d442f79bafa56
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 15:22:49 -0600

    adding controllers to items  route

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,12 +1,21 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+import {
+  deleteItems,
+  getItem,
+  getItems,
+  postItems,
+  updateItems,
+} from "../controllers/item";
 
 const router = Router();
 
 /**
- * * http://localhost:3000/items [GET]
+ * * http://localhost:3000/items 
  */
-router.get("/", (req: Request, res: Response) => {
-  res.send({ data: "Aqui van los models" });
-});
+router.get("/:id", getItem);
+router.get("/", getItems);
+router.post("/", postItems);
+router.put("/:id", updateItems);
+router.delete("/:id", deleteItems);
 
 export { router };
