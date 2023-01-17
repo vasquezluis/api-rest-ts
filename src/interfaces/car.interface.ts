f8eff0977c1407ef6380f4d6934799c7672e0b04
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 14:29:02 -0600

    creating car interface

---
+++ src/interfaces/car.interface.ts
@@ -0,0 +1,14 @@
+/* 
+  * una interface es como un contrato
+  * no tiene logica de programacion 
+  * es una declaracion de una propiedad y tipo de dato
+
+*/
+
+export interface Car {
+  color: string;
+  gas: "gasoline" | "electric";
+  year: number;
+  description: string;
+  price: number;
+}
