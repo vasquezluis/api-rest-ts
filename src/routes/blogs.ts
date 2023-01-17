f1df53d3b0fb8688ac2a7b24ad9c5aa9723f9bc2
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 15:21:42 -0600

    adding controllers to blogs route

--- src/routes/blogs.ts
+++ src/routes/blogs.ts
@@ -1,5 +1,22 @@
 import { Router } from "express";
 
+import {
+  deleteItems,
+  getItem,
+  getItems,
+  postItems,
+  updateItems,
+} from "../controllers/blog";
+
 const router = Router();
+
+/**
+ * * http://localhost:3000/items
+ */
+router.get("/:id", getItem);
+router.get("/", getItems);
+router.post("/", postItems);
+router.put("/:id", updateItems);
+router.delete("/:id", deleteItems);
 
 export { router };
