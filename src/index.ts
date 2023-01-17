f37ff2e45340bfbf1c881ad423a0854efd14c2d4
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 14:26:49 -0600

    adding connection to mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import db from "./config/mongo";
 
 import itemsRoutes from "./routes";
 
@@ -9,6 +10,9 @@ app.use(cors());
 app.use(itemsRoutes);
 
 const PORT = process.env.PORT || 3000;
+
+// conexion a mongo db atlas
+db().then(() => console.log('mongodb connected'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
