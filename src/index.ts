7dee9214848282236bbe4f894b4166f966c3c330
LuiiVasquez <luivasque95@gmail.com>
2023-01-17 13:56:24 -0600

    adding routes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
-const app = express();
+import itemsRoutes from "./routes";
 
+const app = express();
 app.use(cors());
+app.use(itemsRoutes);
 
 const PORT = process.env.PORT || 3000;
 
