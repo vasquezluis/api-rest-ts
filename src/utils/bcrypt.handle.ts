c4c9ca638aff85c55119d8c1579a31a9e3b80574
LuiiVasquez <luivasque95@gmail.com>
2023-01-19 14:51:07 -0600

    creatting encrypt & verify function

---
+++ src/utils/bcrypt.handle.ts
@@ -0,0 +1,15 @@
+// archivo para encriptar contrase;as con bcruptjs
+
+import { hash, compare } from "bcryptjs";
+
+/**
+ * ! Encriptar contrase;a
+ */
+const encrypt = () => {};
+
+/**
+ * ! verificar contrase;a plana con contrase;a encriptada
+ */
+const verified = () => {};
+
+export { encrypt, verified };
