afbbeda7da5c184499602566c98fc700c6169937
LuiiVasquez <luivasque95@gmail.com>
2023-01-19 12:07:54 -0600

    adding middleWare to getItem route (middlewares)

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -6,16 +6,21 @@ import {
   postItems,
   updateItems,
 } from "../controllers/item";
+import { logMiddleware } from "../middlewares/logs";
 
 const router = Router();
 
 /**
- * * http://localhost:3000/items 
+ * * http://localhost:3000/items
  */
-router.get("/:id", getItem);
+router.get("/:id", logMiddleware, getItem);
+
 router.get("/", getItems);
+
 router.post("/", postItems);
+
 router.put("/:id", updateItems);
+
 router.delete("/:id", deleteItems);
 
 export { router };
